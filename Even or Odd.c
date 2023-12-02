7861a388b083b434551843dfe07e49aeb0742f3d
Spoorthi Shet <121347112+Spoorthishet2003@users.noreply.github.com>
2023-12-02 22:27:43 +0530

    Create Even or Odd.c

---
+++ Even or Odd.c
@@ -0,0 +1,16 @@
+//To check weather the given number is even or odd.
+
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    // true if num is perfectly divisible by 2
+    if(num % 2 == 0)
+        printf("%d is even.", num);
+    else
+        printf("%d is odd.", num);
+    
+    return 0;
+}
